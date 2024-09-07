11bb2270a3c4a71ace36a80426828832c9dbcbac
akira-cn <akira.cn@gmail.com>
2024-09-07 08:35:32 +0800

    add more tests

--- src/parser/index.test.ts
+++ src/parser/index.test.ts
@@ -17,3 +17,26 @@ test('default', done => {
   });
   parser.trace(JSON.stringify(_data));
 });
+
+test('one by one', done => {
+  const parser = new JSONParser({
+    parentPath: 'x/y',
+    debug: false,
+  });
+  const _data = {"b": {"a": "你好，我是豆包。"}, "c": 1024, "d": true, "e": null, "f": [1,2,[32], {'g': 'h'}]};
+  const tokens = JSON.stringify(_data).split('');
+
+  parser.on('data', (data) => {
+    console.log(data);
+    expect(data.uri.startsWith('x/y')).toBeTruthy();
+  });
+  parser.on('finish', (data) => {
+    console.log('finish', data);
+    expect(data).toEqual(_data);
+    done();
+  });
+
+  for(let i = 0; i < tokens.length; i++) {
+    parser.trace(tokens[i]);
+  }
+});
