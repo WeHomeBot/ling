808788db4bfcf03c1ed485a5020ee329e27dd56c
akira-cn <akira.cn@gmail.com>
2024-10-14 13:17:33 +0800

    add filter

--- src/tube/index.ts
+++ src/tube/index.ts
@@ -8,6 +8,7 @@ export class Tube extends EventEmitter {
   private _closed: boolean = false;
   private _sse: boolean = false;
   private messageIndex = 0;
+  private filters: ((data: unknown) => boolean)[] = [];
 
   constructor(private session_id: string = shortId()) {
     super();
@@ -19,18 +20,29 @@ export class Tube extends EventEmitter {
     });
   }
 
+  addFilter(filter: ((data: unknown) => boolean) | string | RegExp) {
+    if(typeof filter === 'string') {
+      this.filters.push((data: any) => data.uri === filter);
+    } else if(filter instanceof RegExp) {
+      this.filters.push((data: any) => filter.test(data.uri));
+    } else {
+      this.filters.push(filter);
+    }
+  }
+
   setSSE(sse: boolean) {
     this._sse = sse;
   }
 
   enqueue(data: unknown, isQuiet: boolean = false) {
+    const isFiltered = this.filters.some(filter => filter(data));
     const id = `${this.session_id}:${this.messageIndex++}`;
     if (!this._closed) {
       try {
         if(typeof data !== 'string') {
           if(this._sse && (data as any)?.event) {
             const event = `event: ${(data as any).event}\n`
-            if(!isQuiet) this.controller?.enqueue(event);
+            if(!isQuiet && !isFiltered) this.controller?.enqueue(event);
             this.emit('message', {id, data: event});
           }
           data = JSON.stringify(data) + '\n'; // use jsonl (json lines)
@@ -38,7 +50,7 @@ export class Tube extends EventEmitter {
         if(this._sse) {
           data = `data: ${(data as string).replace(/\n$/,'')}\nid: ${id}\n\n`;
         }
-        if(!isQuiet) this.controller?.enqueue(data);
+        if(!isQuiet && !isFiltered) this.controller?.enqueue(data);
         this.emit('message', {id, data});
       } catch(ex) {
         this._closed = true;
