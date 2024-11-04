db072c0639de13f32416881fb9202e44e7866f6b
akira-cn <akira.cn@gmail.com>
2024-11-04 10:51:12 +0800

    fix error

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -112,8 +112,8 @@ export class ChatBot extends Bot {
     } catch(ex: any) {
       console.error(ex);
       this.chatState = WorkState.ERROR;
-      this.emit('error', ex.message);
       this.tube.enqueue({event: 'error', data: ex.message});
+      this.emit('error', ex.message);
       // this.tube.cancel();
     }
   }
